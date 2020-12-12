6ada8fdeb1f6bca89133cf6a4cca1a02fccc02e3
lauriscx <laurynasriauka98@gmail.com>
2020-12-12 15:17:34 +0200

    Updated camera class.

--- Dimension/src/Engine/Core/Render/Camera.cpp
+++ Dimension/src/Engine/Core/Render/Camera.cpp
@@ -14,6 +14,17 @@ float Camera::zNear = 0.1f;
 float Camera::zFar = 10.0f;
 float Camera::fov = 75.0f;
 
+glm::vec3	Position = glm::vec3(0, 0, 0);
+glm::vec3	ViewDirection = glm::vec3(0, 0, -1);
+glm::vec2	Rotation = glm::vec2(0, 0);
+
+/* Axis rotate on left and right */
+glm::vec3	UpAxis = glm::vec3(0, 1, 0);
+
+/* Proportion rotate up and down */
+glm::vec3	ProportionRotation = glm::vec3(0, 0, 0);
+glm::mat4	ProportionRotationMatrixUpdated = glm::mat4(1);
+
 Camera::Camera() {}
 
 void Camera::SetPerspective(bool perspective) {
@@ -31,17 +42,97 @@ void Camera::SetWindowSize(float width, float height) {
 
 
 glm::mat4 Camera::GetCameraViewMatrix() {
-	return camera * view;
+	return camera * GetLookAt();
 }
 
 void Camera::SetProjectionMatrix() {
-	view = glm::lookAt(glm::vec3(0, 0, 0), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	//view = glm::lookAt(glm::vec3(0, 0, 0), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	if (perspective) {
 		camera = glm::perspective(glm::radians(fov), width / height, zNear, zFar);
 	}
 	else {
 		camera = glm::ortho(0.0f, width, 0.0f, height, zNear, zFar); 
 	}
+}
+
+
+void Camera::SetFieldOfView(float		FieldOfView) {
+	Camera::fov = FieldOfView;
+}
+void Camera::SetViewDirection(glm::vec3	ViewDirection) {
+	Camera::ViewDirection = ViewDirection;
+}
+void Camera::SetViewDistance(glm::vec2	Distance) {
+	Camera::zNear = Distance.x;
+	Camera::zFar = Distance.y;
+}
+void Camera::SetUpAxis(glm::vec3	UpAxis) {
+	Camera::UpAxis = UpAxis;
+}
+void Camera::SetPosition(glm::vec3	Position) {
+	Camera::Position = Position;
+}
+void Camera::SetRotation(glm::vec2	Rotation) {
+	Camera::Rotation = Rotation;
+}
+void Camera::Rotate(glm::vec2	Rotation) {
+	Camera::Rotation = Rotation;
+}
+
+void Camera::CameraMove(glm::vec3 Position, float Speed) {
+	if (Position.x == 1.0f) {
+		ProportionRotation = glm::cross(ViewDirection, UpAxis);
+		Camera::Position += ProportionRotation * glm::vec3(1.0f, 0.0f, 1.0f) * Speed;
+	}
+	if (Position.x == -1.0f) {
+		ProportionRotation = glm::cross(ViewDirection, UpAxis);
+		Camera::Position -= ProportionRotation * Speed;
+
+	}
+	if (Position.z == 1.0f) {
+		Camera::Position += ViewDirection * glm::vec3(1.0f, 0.0f, 1.0f) * Speed;
+	}
+	if (Position.z == -1.0f) {
+		Camera::Position -= ViewDirection * glm::vec3(1.0f, 0.0f, 1.0f) * Speed;
+	}
+	if (Position.y == 1.0f) {
+		Camera::Position += UpAxis * Speed;
+	}
+	if (Position.y == -1.0f) {
+		Camera::Position -= UpAxis * Speed;
+	}
+}
+
+glm::mat4 Camera::GetLookAt() {
+	ProportionRotation = glm::cross(ViewDirection, UpAxis);
+
+	ProportionRotationMatrixUpdated = glm::rotate(glm::mat4(1.0f), -Rotation.x, UpAxis) * glm::rotate(glm::mat4(1.0f), -Rotation.y, ProportionRotation);
+
+	/* Rotation view direction vector */
+	ViewDirection = glm::mat3(ProportionRotationMatrixUpdated) * ViewDirection;
+
+	return glm::lookAt(Position, Position + ViewDirection, UpAxis);
+}
+glm::mat4 Camera::GetPerspective() {
+	return glm::perspective(fov, width / height, zNear, zFar);
+}
+glm::mat4 Camera::GetOrtho() {
+	return  glm::ortho(0.0f, width, 0.0f, height, zNear, zFar);
+}
+glm::vec3 Camera::GetPosition() {
+	return Position;
+}
+glm::vec3 Camera::GetViewDirection() {
+	return ViewDirection;
+}
+glm::vec3 Camera::GetUpAxis() {
+	return UpAxis;
+}
+glm::vec2 Camera::GetRotation() {
+	return Rotation;
+}
+float Camera::GetFieldOfView() {
+	return fov;
 }
 
 Camera::~Camera(){}
--- Dimension/src/Engine/Core/Render/Camera.h
+++ Dimension/src/Engine/Core/Render/Camera.h
@@ -10,6 +10,29 @@ public:
 	static glm::mat4 GetCameraViewMatrix();
 	static void SetProjectionMatrix();
 
+	static glm::mat4 GetLookAt();
+	static glm::mat4 GetPerspective();
+	static glm::mat4 GetOrtho();
+	static glm::vec3 GetPosition();
+	static glm::vec3 GetViewDirection();
+	static glm::vec3 GetUpAxis();
+	static glm::vec2 GetViewAspect();
+	static glm::vec2 GetRotation();
+	static glm::vec2 GetViewDistance();
+	static float GetFieldOfView();
+
+
+
+	static void SetFieldOfView(float		FieldOfView);
+	static void SetViewDirection(glm::vec3	Direction);
+	static void SetViewDistance(glm::vec2	Distance);
+	static void SetUpAxis(glm::vec3	UpAxis);
+	static void SetPosition(glm::vec3	Position);
+	static void SetRotation(glm::vec2	Rotation);
+	static void Rotate(glm::vec2	Rotation);
+
+	static void CameraMove(glm::vec3	Position, float Speed);
+
 	static glm::mat4 camera;
 	static glm::mat4 view;
 	static bool perspective;
@@ -19,8 +42,19 @@ public:
 	static float zFar;
 	static float fov;
 
+	static glm::vec3	Position;
+	static glm::vec3	ViewDirection;
+	static glm::vec2	Rotation;
+
+	/* Axis rotate on left and right */
+	static glm::vec3	UpAxis;
+
+	/* Proportion rotate up and down */
+	static glm::vec3	ProportionRotation;
+	static glm::mat4	ProportionRotationMatrixUpdated;
+
+
 private:
 	Camera();
 	~Camera();
-
 };
